2995305298ae6fb8ebdde5559f3807ea6b957bbc
HLHHS11 <chiryucity@gmail.com>
2023-01-10 16:16:34 +0900

    0110プログラム本体のコミット忘れてたので再度

--- ArrangementOptimization.py
+++ ArrangementOptimization.py
@@ -1,38 +1,68 @@
 import random   #たぶんもうrandomの出番はないけど、一応
 import numpy as np
-#あああああああ
-#いいいいいいい
-
-def generate_chair_array():
-    chair = np.zeros((12,18))
-    for j in range(6):
-        for i in range(4):
-            chair[1+3*i,1+3*j] = 3
-            chair[3*i,3*j] = 2
-            chair[3*i,2+3*j] = 2
-            chair[2+3*i,2+3*j] = 2
-            chair[2+3*i,3*j] = 2
-    return chair
 
 
-chair = generate_chair_array()
+def generate_arrangement_array():
+    """カフェ店内の座席の配置表を生成
+    注意！：表記は数学の行列の表記と同じように見えるが、
+    実際にはlist[][]のように外側から順にインデックスを指定する表記である"""
+    arrangement = np.zeros((12,18))
+    for i in range(4):
+        for j in range(6):
+            arrangement[3*i,3*j] = 2
+            arrangement[3*i,2+3*j] = 2
+            arrangement[1+3*i,1+3*j] = 3
+            arrangement[2+3*i,2+3*j] = 2
+            arrangement[2+3*i,3*j] = 2
+    return arrangement
+
+
+def generate_seat_position_array():
+    seat_position = np.empty((96,2))
+    counter = 0
+    for i in range(4):
+        for j in range(6):
+            seat_position[counter] = [3*i,1+3*j]
+            counter += 1
+            seat_position[counter] = [1+3*i,3*j]
+            counter += 1
+            seat_position[counter] = [1+3*i,2+3*j]
+            counter += 1
+            seat_position[counter] = [2+3*i,1+3*j]
+            counter += 1
+    return seat_position
+
+
+arrangement = generate_arrangement_array()
+seat_position = generate_seat_position_array()
 print("変換前の配置図")
-print(chair)
-#print(chair==0)    このようにすると、TrueとFalseの配列が出力される。けどたぶんいらない
+print(arrangement)
+print(seat_position)
+#print(arrangement==0) 
 
-MAX_NUMBER_OF_1 = 30    #ここから下を追加
-#countNumberOf1 = 0     #この行は消しちゃってOK
-#aaabbb
-positionOfZeroList = []
+#配置図を作成し、それに対する不快度・空間の広さを求める
+#for 
+
+
+
+
+
+"""
+#配列arrangementをもとにして座席図を作る方法
+#次のコミットで消す
+seat_position = []
 for k in range(12):
     for l in range(18):
-        if chair[k,l] == 0:
-            positionOfZeroList.append([k,l])
+        if arrangement[k,l] == 0:
+            seat_position.append([k,l])
 
-random.shuffle(positionOfZeroList)
+#ランダムに0→1置換処理
+MAX_NUMBER_OF_1 = 30    #
+random.shuffle(seat_position)
 for m in range(MAX_NUMBER_OF_1):
-    (row, column) = positionOfZeroList[m]
-    chair[row, column] = 1
+    (row, column) = seat_position[m]
+    arrangement[row, column] = 1
 
-print("変換後の配置図")    
-print(chair)
+#print("変換後の配置図")    
+#print(arrangement)
+"""
