a962712e62098be7468e26fa7d4e03f58309ec90
HLHHS11 <chiryucity@gmail.com>
2023-01-10 13:35:43 +0900

    20230110

--- arrangementOptimization.py
+++ arrangementOptimization.py
@@ -1,19 +1,27 @@
-
+import random   #たぶんもうrandomの出番はないけど、一応
 import numpy as np
-import random   #新しく追加した行
+#あああああああ
 
-chair = np.zeros((12,18))
+def generate_chair_array():
+    chair = np.zeros((12,18))
+    for j in range(6):
+        for i in range(4):
+            chair[1+3*i,1+3*j] = 3
+            chair[3*i,3*j] = 2
+            chair[3*i,2+3*j] = 2
+            chair[2+3*i,2+3*j] = 2
+            chair[2+3*i,3*j] = 2
+    return chair
 
-for j in range(6):
-    for i in range(4):
-        chair[1+3*i,1+3*j] = 3
-        chair[3*i,3*j] = 2
-        chair[3*i,2+3*j] = 2
-        chair[2+3*i,2+3*j] = 2
-        chair[2+3*i,3*j] = 2
+
+chair = generate_chair_array()
+print("変換前の配置図")
+print(chair)
+#print(chair==0)    このようにすると、TrueとFalseの配列が出力される。けどたぶんいらない
 
 MAX_NUMBER_OF_1 = 30    #ここから下を追加
 #countNumberOf1 = 0     #この行は消しちゃってOK
+#aaabbb
 positionOfZeroList = []
 for k in range(12):
     for l in range(18):
@@ -24,5 +32,6 @@ random.shuffle(positionOfZeroList)
 for m in range(MAX_NUMBER_OF_1):
     (row, column) = positionOfZeroList[m]
     chair[row, column] = 1
-    
+
+print("変換後の配置図")    
 print(chair)
